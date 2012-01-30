ac6528be2934a5f21dc1cf65a99e3451a2408b83
alex@alexsmith.me <alex@alexsmith.me@54734666-6f3c-0410-830a-cf3c43b2062a>
2012-01-30 18:22:02 +0000

    Revision M Beta Version

--- EveMarketTool/Code/Constants.cs
+++ EveMarketTool/Code/Constants.cs
@@ -12,17 +12,17 @@ namespace EveMarketTool
     public class Constants
     {
         // public readonly static string EveVersionName = "Dominion";
-        public readonly static bool IsBetaVersion = false;
+        public readonly static bool IsBetaVersion = true;
 
         public readonly static int dbg_trips_arg = 2;
 
         public readonly static string BetaTooltipMessage =
             "This is a beta version of NavBot. It may crash, be inaccurate, or not work at all. Use at your own ISK risk!";
 
-        public readonly static string Revision = "L (Release)";
+        public readonly static string Revision = "M (Beta)";
         public readonly static string Credits = 
             "NavBot was written by Tejar. Contributions have been made generously by: Mark O'Connor, " +
             "Michael Votteler, Bart Seresia, Michael Mitchell, munged92, Michael Hollnbuchner, ok123jump, " +
-            "jlstrobel (John Strobel), rigbyd, Terracarbon (Terry Winfrey), Papa Yoru (Tony Jackson) and others.";
+            "jlstrobel (John Strobel), rigbyd, Terracarbon (Terry Winfrey), Papa Yoru (Tony Jackson), Mikal Red and others.";
     }
 }
